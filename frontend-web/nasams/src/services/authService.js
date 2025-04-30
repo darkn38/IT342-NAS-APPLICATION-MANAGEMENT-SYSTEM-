cfd43d482dbf889ea6471843fbf3c6ac55cca552
darkn38 <cjaymknight@gmail.com>
2025-04-30 09:12:32 +0800

    Fix: Added react-router-dom and updated dependencies

--- frontend-web/nasams/src/services/authService.js
+++ frontend-web/nasams/src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/authService.js
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/auth';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const loginAdmin = async (credentials) => {
     try {
