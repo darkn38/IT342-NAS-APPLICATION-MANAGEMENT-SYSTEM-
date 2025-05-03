22f418417d4760dbbe5b2e580345476cfe6283d6
Jan Elseph Yu <janelsephyu@gmail.com>
2025-05-03 15:16:21 +0800

    Changed JwtFilter.java

--- backend/nasamanagementsystem/src/main/java/edu/cit/nasamanagementsystem/security/jwt/JwtFilter.java
+++ backend/nasamanagementsystem/src/main/java/edu/cit/nasamanagementsystem/security/jwt/JwtFilter.java
@@ -1,10 +1,7 @@
 package edu.cit.nasamanagementsystem.security.jwt;
 
-import edu.cit.nasamanagementsystem.service.CustomUserDetailsService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +10,11 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import edu.cit.nasamanagementsystem.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -30,10 +31,10 @@ public class JwtFilter extends OncePerRequestFilter {
 
         final String requestPath = request.getServletPath();
 
-        if (requestPath.equals("/api/auth/login") || requestPath.equals("/api/users/register")) {
+        if (requestPath.equals("/api/auth/login") || requestPath.equals("/api/auth/register")|| requestPath.equals("/api/users/register")) {
             filterChain.doFilter(request, response);
             return;
-        }
+    }
 
         final String authHeader = request.getHeader("Authorization");
         String username = null;
